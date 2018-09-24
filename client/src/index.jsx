34f127c01132b89e1a458dbddd45859ef2203351
Alex Rahgozar <AlexRahgozar@gmail.com>
2018-09-24 07:13:19 -0700

    complete Navbar

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,2 +1,106 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import Rewview from './components/Review.jsx';
+import $ from 'jquery';
+import Navbars from './components/Navbars.jsx'
+import SignBoot from './components/SignBoot.jsx'
+import Carouselz from './components/Carouselz.jsx'
+import SignUp from './components/SignUp.jsx'
+import RevForm from './components/RevForm.jsx'
+
+import { Container, Row, Col } from 'react-bootstrap'
+
+// import reviewData from './reviewData.js';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      info: []
+    }
+    this.save = this.save.bind(this);
+    this.saveReview = this.saveReview.bind(this)
+  }
+
+
+  componentDidMount() {
+    $.get('/login', function(signInData) {
+      console.log('This is signUp inside index.jsx GET:  ', signInData)
+    })
+    .done(signInData => {
+      this.setState({
+        info: signInData
+      });
+    });
+  }
+
+  save(data) {
+    console.log('this is save Data: ', data);
+
+    fetch('/login', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    })
+    .then(() => window.location.reload(false))
+  }
+
+
+    // activateReview () {
+    //   $.get("/review", function(review) {
+    //     console.log(review)
+    //   })
+    // }
+
+    componentDidMount() {
+      $.get('/review', function(signInData) {
+        console.log('This is Review index.jsx GET:  ', signInData)
+      })
+      .done(signInData => {
+        this.setState({
+          info: signInData
+        });
+      });
+    }
+    //
+
+
+
+    saveReview(data) {
+      console.log('this is save Inside SaveReview Data: ', data);
+      fetch('/review', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: new Headers({
+          'Content-Type': 'application/json'
+        })
+      })
+      .then(() => window.location.reload(false))
+    }
+    //
+    // <RevForm saveRev={this.saveReview}/>
+    // <button onClick={this.activateReview}>Review</button>
+    // <SignUp />
+      // <button onClick={this.activateReview}>Write A Review</button>
+  render() {
+
+    return (
+      <div>
+        <Navbars/>
+          <Row>
+            <Col md={6} sm={6}> <SignBoot/> </Col>
+            <Col md={5} md={'offset'}> <Carouselz/> </Col>
+          </Row>
+          <RevForm saveRev={this.saveReview}/>
+          <SignUp saveInfo={this.save}/>
+
+      </div>
+    )
+  }
+
+}
+
+
+ReactDOM.render(<App/>, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 var path = require('path');
 var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist');
-
+ // import css from 'file.css';
+// const css = require('./test.css').toString();
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
@@ -9,15 +10,64 @@ module.exports = {
     path: DIST_DIR
   },
   module : {
-    loaders : [
+    rules : [
       {
         test : /\.jsx?/,
         include : SRC_DIR,
         loader : 'babel-loader',
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'env']
        }
-      }
+     },
+     {
+       test: /\.(gif|png|jpe?g|svg)/i,
+       use: [
+         'file-loader',
+         {
+           loader: 'image-webpack-loader',
+           options: {
+             gifsicle: {
+               interlanced: false
+             },
+             optipng: {
+               optimizationLevel : 7
+             },
+             pngquant: {
+               quality: '69-90',
+               speed: 4
+             },
+             mozjpeg: {
+               progressive: true,
+               quality: 65
+             }
+
+           }
+         }
+       ]
+     }
     ]
   }
 };
+//
+// module.exports = {
+//   entry:  `${SRC_DIR}/uc2.jpg`,
+//   output: {
+//     filename: 'bundle.js',
+//     path: DIST_DIR
+//   },
+//   module : {
+//     rules: [
+//       {
+//         test: /\.css$/,
+//         use: [ 'style-loader', 'css-loader' ]
+//       },
+//       {
+//         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+//         loader: 'url-loader',
+//         options: {
+//           limit: 10000
+//         }
+//       }
+//     ]
+//   }
+// };
